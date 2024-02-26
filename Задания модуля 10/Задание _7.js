157af312072dc6cd7e866fe9cded73e565919810
PavelKazmin <150178582+PavelKazmin@users.noreply.github.com>
2024-02-26 20:14:51 +0300

    Update Задание _7.js
    переделал задание, добавлен подсчет нулевых элементов

--- Задания модуля 10/Задание _7.js
+++ Задания модуля 10/Задание _7.js
@@ -6,13 +6,16 @@ const arrEvenOdd = [18, 335, 876, 348, 12, 90, 465, 45, 78, 54, 3456, 1, 93, 8, 
 
 let countEven = 0,
 	countOdd = 0;
+	countZeros= 0;
 
 for (let i = 0; i < arrEvenOdd.length; i++) {
 	if (arrEvenOdd[i] % 2 === 0) {
 		countEven++;
 	} else if (arrEvenOdd[i] % 2 !== 0) {
 		countOdd++;
+	} else if (arrEvenOdd[i] === 0) {
+		countZeros++;
 	}
 }
 
-console.log("Четных - " + countEven + "," + " Нечетных - " + countOdd + ", Нулевой элемент - " + arrEvenOdd[0]);
+console.log("Четных - " + countEven + "," + " Нечетных - " + countOdd + ", Нулевых элементов - " + countZeros);
