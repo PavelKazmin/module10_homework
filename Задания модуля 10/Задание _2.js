89d8365fdbffbd782374d6e75413b1755c9e12a6
PavelKazmin <150178582+PavelKazmin@users.noreply.github.com>
2024-03-24 00:30:49 +0300

    Добавил switch

--- Задания модуля 10/Задание _2.js
+++ Задания модуля 10/Задание _2.js
@@ -2,6 +2,7 @@
 
 //Задание2
 
+(if-else)
 let x = 0
 
 if (x === Number || String || Boolean) {
@@ -9,3 +10,21 @@ if (x === Number || String || Boolean) {
 } else {
     console.log ("Тип x не определен")
 }
+
+(switch)
+ let x =[1, 2, 3, 3, 4, 4, 4, 4, 4];
+
+switch (typeof x) {
+    case 'number':
+        console.log('x - число');
+        break;
+    case 'string':
+        console.log('x - строка');
+        break;
+    case 'boolean':
+        console.log('x - логический тип');
+        break;
+    default:
+        console.log('Тип x не определён');
+        break;
+}   
