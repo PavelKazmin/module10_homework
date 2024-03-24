a7bda0a637d721550b0c95910c8e46f3df349183
PavelKazmin <150178582+PavelKazmin@users.noreply.github.com>
2024-03-24 21:30:07 +0300

    Update Задание _4.js

--- Задания модуля 10/Задание _4.js
+++ Задания модуля 10/Задание _4.js
@@ -9,4 +9,11 @@ function randomInt(min, max) {
 
   let randomNum = randomInt(1, 100);
 
-  console.log(randomNum)
+  console.log(randomNum);
+
+
+
+const random = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+console.log(random(0, 100));
